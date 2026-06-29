328bcfe99d45beb46a530442530ac121b215b3eb
Sameer Kumar <smyel4832@gmail.com>
2026-06-29 14:04:09 +0530

    Added 1445C

---
+++ 1455C_ping_pong/1455C.cpp
@@ -0,0 +1,21 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+#define ll long long
+#define pb push_back
+
+int main() {
+    ios::sync_with_stdio(false);
+    cin.tie(NULL);
+
+    int t;
+    cin >> t;
+    while (t--) {
+        int x, y;
+        cin >> x >> y;
+
+        cout << x - 1 << " " << y << endl;
+    }
+
+    return 0;
+}
